37eef922f7eb0258d72a7ed6cb328c43c1c11fb6
jrdutra <jrdutra.com.br@gmail.com>
2020-09-16 15:05:56 -0300

    File IO Dev

--- fileIO.py
+++ fileIO.py
@@ -1,8 +1,17 @@
 
 
 class FileIO:
-    def __init__(self, path, fileType=".txt"):
+    def __init__(self, path, file_type=".txt"):
         self.path = path
-        self.fileType = fileType
+        self.file_type = file_type
     
+    def read_as_vector(self):
+        vec = []
+        with open(self.path) as inp:
+            for line in inp:
+                line = self.remove_new_line_caracters(line)
+                vec.append(line)
+        return vec
     
+    def remove_new_line_caracters(self, str):
+        return str.replace('\n', '')
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from fileIO import FileIO
 
 def main():
     print("Iniciando...")
+    file = FileIO("./test.txt")
+    vec = file.read_as_vector()
+    print(vec)
 
 if __name__ == '__main__':
     main()
